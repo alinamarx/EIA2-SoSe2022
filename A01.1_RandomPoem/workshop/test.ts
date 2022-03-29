fd8e94222714d1e00de284feb3256f9175c6e864
alinamarx <alina.marx@hs-furtwangen.de>
2022-03-29 10:37:04 +0200

    Repository Link

--- A01.1_RandomPoem/workshop/test.ts
+++ A01.1_RandomPoem/workshop/test.ts
@@ -1,25 +1,25 @@
 let v: number = 1;
-v = v + "1";
+v = v + 1;
 console.log(v);
 
 const w: number = 1;
-w = w + 1;
+//w = w + 1;
 console.log(w);
 
-let a = [7, true, "Hallo"];
+let a [] = [7, true, "Hallo"];
 //console.log(a);
 //console.log(a[0]);
 //console.log(a[3]);
 
-a[4] = [101, 102];
-console.log(a[3]);
-console.log(a[5]);
+//a[4] = [101, 102];
+//console.log(a[3]);
+//console.log(a[5]);
 
 let s = {"zahl": 7, "wahr": true, text: "Hallo"};
 console.log(s);
 console.log(s["zahl"]);
 console.log(s.zahl);
-console.log(s.geburstag);
+//console.log(s.geburtstag);
 
 s[4] = [101];
 console.log(s);
@@ -28,7 +28,7 @@ console.log(s[4]);
 interface Daten {
     Name: string;
     alter: number;
-    geimpft: [ja: number]: boolean;
+    geimpft: [ja: number]; //:boolean
 }
 
 let studenten: Daten [] = [];
@@ -43,12 +43,12 @@ console.log(isDivisible(4, 3));
 console.log(isDivisible(4, 2));
 isDivisible(10, 5);
 
-let v1 = 1;
-let v2 = v1;
+let v1: number = 1;
+let v2: number = v1;
 v1 = 3;
 console.log(v1, v2);
 
-let v3 [] = [1, 2, 3];
-let v4 = v3;
+let v3: number [] = [1, 2, 3];
+let v4: number [] = v3;
 v3 = [3, 2, 1];
 console.log(v3, v4);
