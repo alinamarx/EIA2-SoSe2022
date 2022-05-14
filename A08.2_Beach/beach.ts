a73b058a8d367e074a571490bc241b4b193db67c
alinamarx <alina.marx@hs-furtwangen.de>
2022-05-14 23:18:53 +0200

    Aufgabe 08.2

--- A08.2_Beach/beach.ts
+++ A08.2_Beach/beach.ts
@@ -25,14 +25,19 @@ namespace CanvasBeach {
         drawClouds({ x: 1050, y: 50 });
 
         drawBeach();
-        drawPlants({x: 1000, y: 200});
-        drawPlants({x: 910, y: 230});
-        drawPlants({x: 1300, y: 270});
+        drawPlants({ x: 1000, y: 200 });
+        drawPlants({ x: 910, y: 230 });
+        drawPlants({ x: 1300, y: 270 });
 
-        drawPeople("swimming", {x: 200, y: 200});
-        drawPeople("surfing", {x: 100, y: 100});
-        drawPeople("sunbathing", {x: 400, y: 400});
-        // drawBirds();
+        drawPeople("swimming", { x: 150, y: 280 });
+        drawPeople("swimming", { x: 210, y: 265 });
+        drawPeople("swimming", { x: 500, y: 500 });
+        drawPeople("swimming", { x: 460, y: 430 });
+        drawPeople("surfing", { x: 120, y: 480 });
+        drawPeople("surfing", { x: 200, y: 380 });
+        drawPeople("sunbathing", { x: 920, y: 410 });
+        drawPeople("sunbathing", { x: 960, y: 440 });
+        drawBirds();
     }
 
     function colorBackground(): void {
@@ -47,7 +52,7 @@ namespace CanvasBeach {
         let ocean: CanvasGradient = crc2.createLinearGradient(0, 0, 0, 700);
         ocean.addColorStop(0.28, "white");
         ocean.addColorStop(0.38, "#ff781f");
-        ocean.addColorStop(0.47, "#0B2275");
+        ocean.addColorStop(0.5, "HSL(240, 50%, 45%)");
         ocean.addColorStop(0.8, "#4C125E");
 
         crc2.fillStyle = ocean;
@@ -134,14 +139,14 @@ namespace CanvasBeach {
 
     function drawPlants(_position: Vector): void {
 
-        let nStrands: number = 50;
+        let nStrands: number = 70;
         let strand: Path2D = new Path2D();
 
         crc2.fillStyle = "#285D34";
         crc2.lineWidth = 10;
 
         strand.moveTo(5, 0);
-        strand.lineTo(-2, 28);
+        strand.lineTo(-4, 28);
         strand.lineTo(3, 25);
         strand.moveTo(0, 0);
         strand.ellipse(6, -3, 2, 6, Math.PI / 11, 0, 2 * Math.PI);
@@ -151,26 +156,160 @@ namespace CanvasBeach {
 
         for (let drawn: number = 0; drawn < nStrands; drawn++) {
 
-        crc2.save();
-        let x: number = (Math.random() - 0.5) * 750;
-        let y: number = (Math.random() - 0.5) * 35;
-        crc2.translate(x, y);
-        crc2.fill(strand);
-        crc2.restore();
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * 750;
+            let y: number = (Math.random() - 0.5) * 35;
+            crc2.translate(x, y);
+            crc2.fill(strand);
+            crc2.restore();
         }
 
         crc2.restore();
 
     }
 
-    function drawPeople (state: string, _position: Vector): void {
-        
+    function drawPeople(state: string, _position: Vector): void {
+
         if (state == "swimming") {
+            crc2.save();
+            crc2.translate(_position.x, _position.y);
+            crc2.save();
+
+            crc2.beginPath();
+            crc2.rotate(-45);
+            crc2.moveTo(0, 0);
+            crc2.lineTo(0, -15);
+            crc2.rotate(45);
+            crc2.restore();
+            crc2.save();
+
+            crc2.rotate(15);
+            crc2.moveTo(0, 0);
+            crc2.lineTo(-15, 0);
+            crc2.rotate(-15);
+            crc2.restore();
+            crc2.save();
+
+            crc2.moveTo(0, 0);
+            crc2.lineTo(0, -8);
+            crc2.closePath();
+            crc2.restore();
+
+            crc2.translate(0, -8);
+            crc2.moveTo(5, -4);
+            crc2.arc(0, -4, 5, 0, 2 * Math.PI);
+
+            crc2.lineWidth = 2;
+            crc2.fillStyle = "black";
+            crc2.stroke();
+            crc2.fill();
+            crc2.restore();
 
         } else if (state == "surfing") {
 
+            crc2.save();
+            crc2.translate(_position.x, _position.y);
+            crc2.save();
+
+            let surfboard: Path2D = new Path2D();
+            surfboard.ellipse(1, 2, 10, 30, Math.PI / 3, 0, 2 * Math.PI);
+            crc2.fillStyle = "#BCE3B9";
+            crc2.fill(surfboard);
+            crc2.restore();
+
+            let person: Path2D = new Path2D();
+            person.moveTo(0, -10);
+            person.lineTo(8, 6);
+
+            person.moveTo(0, -10);
+            person.lineTo(-8, 4);
+
+            person.moveTo(0, -10);
+            person.lineTo(0, -20);
+
+            person.moveTo(0, -20);
+            person.lineTo(-10, -9);
+            person.moveTo(0, -20);
+            person.lineTo(10, -6);
+
+            person.moveTo(5, -26);
+            person.arc(0, -26, 5, 0, 2 * Math.PI);
+
+            crc2.lineWidth = 2;
+            crc2.stroke(person);
+            crc2.fillStyle = "black";
+            crc2.fill(person);
+
+            crc2.restore();
         } else if (state == "sunbathing") {
 
+            crc2.save();
+            crc2.translate(_position.x, _position.y);
+
+            let towel: Path2D = new Path2D();
+            towel.moveTo(-18, -4);
+            towel.lineTo(23, -26);
+            towel.lineTo(39, -7);
+            towel.lineTo(-5, 16);
+
+            crc2.fillStyle = "#83DCDD";
+            crc2.fill(towel);
+
+            let person: Path2D = new Path2D();
+            person.moveTo(0, -10);
+            person.lineTo(8, 6);
+
+            person.moveTo(0, -10);
+            person.lineTo(-8, 4);
+
+            person.moveTo(0, -10);
+            person.lineTo(0, -20);
+
+            person.moveTo(0, -20);
+            person.lineTo(-10, -9);
+            person.moveTo(0, -20);
+            person.lineTo(10, -6);
+
+            person.moveTo(5, -26);
+            person.arc(0, -26, 5, 0, 2 * Math.PI);
+
+            crc2.rotate(45);
+            crc2.lineWidth = 2;
+            crc2.stroke(person);
+            crc2.fillStyle = "black";
+            crc2.fill(person);
+
+            crc2.restore();
         }
+    }
+
+    function drawBirds(): void {
+        crc2.save();
+        crc2.translate(canvas.width / 2, 100);
+
+        let nBirds: number = 8;
+        let bird: Path2D = new Path2D;
+        bird.moveTo(0, 0);
+        bird.lineTo(-10, -10);
+        bird.lineTo(-20, 0);
+        bird.moveTo(0, 0);
+        bird.lineTo(10, -10);
+        bird.lineTo(20, 3);
+
+        crc2.lineWidth = 4;
+        crc2.lineCap = "round";
+        crc2.lineJoin = "round";
+
+        for (let drawn: number = 0; drawn < nBirds; drawn++) {
+
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * canvas.width;
+            let y: number = (Math.random() - 0.5) * canvas.height / 2;
+            crc2.translate(x, y);
+            crc2.stroke(bird);
+            crc2.restore();
+        }
+
+        crc2.restore();
     }
 }
